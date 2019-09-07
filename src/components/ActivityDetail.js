f11e09184fb507bdf042d03985026b596c6065a6
Chris Carr <christocarr@outlook.com>
2019-09-07 20:32:37 +0100

    Add styling

--- src/components/ActivityDetail.js
+++ src/components/ActivityDetail.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React from "react"
 
 const ActivityDetail = ({ results }) => {
+
   const renderedList = results.map((obj, index) => {
+    console.log(obj)
     return (
       <li key={index}>
-        <div className="service-name-container">
+        <div className="service-container">
           <h4>{obj.NameOfService}</h4>
-          <p>{obj.AddressLine1}</p>
-          <p>{obj.AddressLine2}</p>
-          <p>{obj.AddressLine3}</p>
-          <p>{obj.Postcode}</p>
+          <div className="address">
+            <p>{obj.AddressLine1}</p>
+            <p>{obj.AddressLine2}</p>
+            <p>{obj.AddressLine3}</p>
+            <p>{obj.Postcode}</p>
+          </div>
+          <p>{obj.OtherDetailedInformation}</p>
+          <div className="times">
+            <p>{}</p>
+          </div>
+          <p>Cost: {obj.Cost}</p>
+          <div className="transport">
+            <p className="section-heading">Transport:</p>
+            <p>Buses: {obj.Buses}</p>
+            <p>Tube and Trains: {obj.TubeAndTrains}</p>
+            <p>Car Park Availability: {obj.CarParkingDetails}</p>
+          </div>
+          <div className="accessibility">
+            <p>Accessibility: {obj.AccessibilityDetails}</p>
+          </div>
+          <div className="contact-details">
+            <p className="section-heading">Contact Details:</p>
+            <div className="name-number"><p>{obj.Name1}:</p><p>{obj.PhoneNumber1};</p><p>{obj.Email1}; {obj.Email2}</p>
+            </div>
+            <a href="{obj.Website}">{obj.Website}</a>
+          </div>
         </div>
       </li>
     );
@@ -17,7 +41,9 @@ const ActivityDetail = ({ results }) => {
 
   return (
     <div>
-      <ul>{renderedList}</ul>
+      <ul>
+        {renderedList}
+      </ul>
     </div>
   );
 };
