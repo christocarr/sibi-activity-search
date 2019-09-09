13985227e031ac3c26cd2ee32632f8a32290b215
christocarr <christocarr@outlook.com>
2019-09-09 16:26:10 +0100

    Modify capitalistion

--- src/components/ActivityCatSelect.js
+++ src/components/ActivityCatSelect.js
@@ -2,9 +2,9 @@ import React from "react";
 import Select from "react-select";
 
 const activityCatOptions = [
-  { value: "Activity (art & craft)", label: "art & craft" },
-  { value: "Activity (social)", label: "social" },
-  { value: "Activity (wellbeing)", label: "wellbeing" }
+  { value: "Activity (art & craft)", label: "Art & craft" },
+  { value: "Activity (social)", label: "Social" },
+  { value: "Activity (wellbeing)", label: "Wellbeing" }
 ];
 
 const ActivityCatSelect = ({ handleChange }) => {
