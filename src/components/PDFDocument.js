32195e973737ec1b95ec645e199eaa5c3a4ece0a
Chris Carr <christocarr@outlook.com>
2019-09-15 17:26:36 +0100

    add style component

--- src/components/PDFDocument.js
+++ src/components/PDFDocument.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import { PDFViewer, Page, Text, View, Document } from '@react-pdf/renderer'
+import { PDFViewer, Page, Text, View, Document,StyleSheet } from '@react-pdf/renderer'
 
-const PDFDocument = () => (
-  <PDFViewer className="pdf-viewer">
-    <Document>
-      <Page size="A4">
-        <View>
-          <Text>PDF Text</Text>
-        </View>
-      </Page>
-    </Document>
-  </PDFViewer>
-)
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+})
+
+const PDFDocument = (props) => {
+  console.log(props)
+  return(
+    <PDFViewer className="pdf-viewer">
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text>
+              <h4>{props.NameOfService}</h4>
+            </Text>
+          </View>
+        </Page>
+      </Document>
+    </PDFViewer>
+  )  
+}
 
 export default PDFDocument
