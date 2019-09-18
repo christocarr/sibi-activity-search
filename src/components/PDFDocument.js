d8f65443f48131d884d2017b7d2b49dd088b65ff
Chris Carr <christocarr@outlook.com>
2019-09-18 20:21:02 +0100

    Add pdf views

--- src/components/PDFDocument.js
+++ src/components/PDFDocument.js
@@ -1,27 +1,81 @@
 import React from 'react'
 import { PDFViewer, Page, Text, View, Document,StyleSheet } from '@react-pdf/renderer'
 
+const data = localStorage.getItem('PDFData')
+const PDFData = JSON.parse(data)
+
+console.log(PDFData)
+
+const { NameOfService, AccessibilityDetails, AddressLine1, AddressLine2, AddressLine3, Postcode, OtherDetailedInformation, MondayStart, MondayEnd, TuesdayStart, TuesdayEnd, WednesdayStart, WednesdayEnd, ThursdayStart, ThursdayEnd, FridayStart, FridayEnd, SaturdayStart, SaturdayEnd, SundayStart, SundayEnd, Cost,  Buses, TubeAndTrains, CarParkingDetails, Name1, PhoneNumber1, Email1, Name2, PhoneNumber2, Email2, Website, OtherContactInfo  } = PDFData
+
+
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
+    flexDirection: 'column',
+    padding: 30,
+    fontFamily: 'Helvetica'
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
+  heading: {
+    marginTop: 10,
+    fontFamily: 'Helvetica-Bold'
+  },
+  // subHeading: {
+  //   fontFamily: 'Helvetica-Bold'
+  // },
+  text: {
+    marginTop: 10,
+    fontSize: 14,
   }
 })
 
-const PDFDocument = ({ data }) => {
-  console.log(data)
+const PDFDocument = () => {
   return(
     <PDFViewer className="pdf-viewer">
       <Document>
         <Page size="A4" style={styles.page}>
-          <View style={styles.section}>
-            <Text>
-              Name of Service
-            </Text>
+          <View style={styles.heading}>
+            <Text>{NameOfService}</Text>
+          </View>
+          <View style={styles.text}>
+            <Text>{AddressLine1}</Text>
+            <Text>{AddressLine2}</Text>
+            <Text>{AddressLine3}</Text>
+            <Text>{Postcode}</Text>
+          </View>
+          <View style={styles.text}>
+            <Text>{OtherDetailedInformation}</Text>
+          </View>
+          <View style={styles.text}>
+            {MondayStart ? <Text>Monday: {MondayStart} - {MondayEnd}</Text> : null}
+            {TuesdayStart ? <Text>Tuesday: {TuesdayStart} - {TuesdayEnd}</Text> : null}
+            {WednesdayStart ? <Text>Wednesday: {WednesdayStart} - {WednesdayEnd}</Text> : null}
+            {ThursdayStart ? <Text>Thursday: {ThursdayStart} - {ThursdayEnd}</Text> : null}
+            {FridayStart ? <Text>Friday: {FridayStart} - {FridayEnd}</Text> : null}
+            {SaturdayStart ? <Text>Saturday: {SaturdayStart} - {SaturdayEnd}</Text> : null}
+            {SundayStart ? <Text>Sunday: {SundayStart} - {SundayEnd}</Text> : null}
+          </View>
+          <View style={styles.text}>
+            <Text>Cost: {Cost}</Text>
+          </View>
+          <View style={styles.text}>
+            <Text>Transport:</Text>
+            <Text>Buses: {Buses}</Text>
+            <Text>Tube and Trains: {TubeAndTrains}</Text>
+            <Text>Car park Availibility: {CarParkingDetails}</Text>
+          </View>
+          <View style={styles.text}>
+            {AccessibilityDetails ? <Text>Accessibility: {AccessibilityDetails}</Text> : null}
+          </View>
+          <View style={styles.text}>
+            <Text>Contact Details:</Text>
+            <Text>{Name1}: {PhoneNumber1}; {Email1}</Text>
+            {Name2 ? <Text>{Name2}: {PhoneNumber2}; {Email2}</Text> : null}
+          </View>
+          <View style={styles.text}>
+            {Website ? <Text>{Website}</Text> : null}
+          </View>
+          <View style={styles.text}>
+            {OtherContactInfo ? <Text>{OtherContactInfo}</Text> : null}
           </View>
         </Page>
       </Document>
