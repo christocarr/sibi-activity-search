54f72d4a6861f27babbee55dd2735474ea3138e7
Chris Carr <christocarr@outlook.com>
2019-09-19 21:34:02 +0100

    Add categories

--- src/components/ActivityCatSelect.js
+++ src/components/ActivityCatSelect.js
@@ -4,7 +4,12 @@ import Select from "react-select";
 const activityCatOptions = [
   { value: "Activity (art & craft)", label: "Art & craft" },
   { value: "Activity (social)", label: "Social" },
-  { value: "Activity (wellbeing)", label: "Wellbeing" }
+  { value: "Activity (wellbeing)", label: "Wellbeing" },
+  { value: "Activity (board & card games)", label: "Board and card games" },
+  { value: "Activity (music & drama)", label: "Music and drama" },
+  { value: "Activity (sport & active games)", label: "Sports and active games" },
+  { value: "Learning topic", label: "Learning" },
+  { value: "Advice", label: "Advice" }
 ];
 
 const ActivityCatSelect = ({ handleChange }) => {
